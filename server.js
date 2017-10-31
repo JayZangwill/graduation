ddc835859be855a31557492a10769b15b3608462
Jay Zangwill <359390851@qq.com>
2017-10-31 18:40:22 +0800

    环境配置

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const fs = require('fs'),
     history = require('connect-history-api-fallback'),
     staticFileMiddleware = express.static(__dirname);
 let html = fs.readFileSync('index.html').toString();
+
+// index.html的style引入
 if (!/<link rel=\"stylesheet\" href=\"dist\/css\/main.css\">/.test(html)) {
     html = html.replace(/<title>(\w+)<\/title>/, '<title>$1</title>\n\t<link rel="stylesheet" href="dist/css/common.css">\n\t<link rel="stylesheet" href="dist/css/main.css">');
     fs.writeFileSync('index.html', html);
 }
 
 app.use(compression());
+// 配合vue-router
 app.use(staticFileMiddleware);
 app.use(history({
     disableDotRule: true,
